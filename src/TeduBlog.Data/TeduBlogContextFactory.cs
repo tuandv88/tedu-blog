440a477bce3a9e4216472f9572e0fcc417920b6e
TuanDv <vinhtuan9668@gmail.com>
2024-05-09 22:43:00 +0700

    Apply ASPNet Core Identity and config migrate database.

---
+++ src/TeduBlog.Data/TeduBlogContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduBlog.Data
+{
+	public class TeduBlogContextFactory : IDesignTimeDbContextFactory<TeduBlogContext>
+	{
+		public TeduBlogContext CreateDbContext(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var builder = new DbContextOptionsBuilder<TeduBlogContext>();
+			builder.UseSqlServer(configuration.GetConnectionString("DB"));
+			return new TeduBlogContext(builder.Options);
+		}
+	}
+}
